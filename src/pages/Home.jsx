35997e5407b40863d28d0c3638fc85c4b7ec2eb8
BGC <uthmanajanaku@gmail.com>
2025-05-05 15:35:17 +0100

    React Tutorials

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Recipe from "../components/Recipe";
+import SimpleRef from "../components/SimpleRef";
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <Recipe />
+      <SimpleRef />
     </div>
   );
 };
